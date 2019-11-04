2502203e6184d17b300c412c0ff18e216f161180
Michael Weedman <michael.weedman@gmail.com>
2019-11-04 09:34:26 -0700

    Step one completed

--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ import './index.css';
 import shoppingList from './shopping-list';
 
 const main = function () {
-  fetch('https://thinkful-list-api.herokuapp.com/ei-student/items')
-  .then(res => res.json())
-  .then(data => console.log(data));
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
