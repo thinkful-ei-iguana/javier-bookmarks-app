981b8a37494f40eccaa78dc30fa0c9a28afd302e
Michael Weedman <michael.weedman@gmail.com>
2019-11-04 10:03:21 -0700

    Snagged on step 4/5

--- src/index.js
+++ src/index.js
@@ -2,10 +2,26 @@ import $ from 'jquery';
 import api from './api';
 import 'normalize.css';
 import './index.css';
+import store from './store.js';
 
 import shoppingList from './shopping-list';
-
+api.createItem('Pears');
 const main = function () {
+  // api.createItem('pears')
+  //   .then(res => res.json())
+  //   .then((newItem) => {
+  //     return api.getItems();
+  //   })
+  //   .then(res => res.json())
+  //   .then((items) => {
+  //     console.log(items);
+  //   });
+  api.getItems()
+    .then(res => res.json())
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
   shoppingList.bindEventListeners();
   shoppingList.render();
 };
