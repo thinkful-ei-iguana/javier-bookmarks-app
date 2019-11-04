e3fe143f9065db3653686b8fc823aef475989bda
Michael Weedman <michael.weedman@gmail.com>
2019-11-04 12:37:20 -0700

    Part 8 - Not working

--- src/index.js
+++ src/index.js
@@ -5,25 +5,33 @@ import './index.css';
 import store from './store.js';
 
 import shoppingList from './shopping-list';
-api.createItem('Pears');
+// api.createItem('Pears');
 
-api.getItems()
-  .then(res => res.json())
-  .then((items) => {
-    console.log(items)
-    items.forEach((item) => store.addItem(item));
-    shoppingList.render();
-  });
+// api.getItems()
+//   .then(res => res.json())
+//   .then((items) => {
+//     console.log(items);
+//     items.forEach((item) => store.addItem(item));
+//     shoppingList.render();
+//   });
 
+
+
+const main = function () {
   api.getItems()
     .then(res => res.json())
     .then((items) => {
       const item = items[0];
+      console.log(item.name);
       return api.updateItem(item.id, { name: 'foobar' });
     })
-    .then(res => res.json())
+  // .then(res => res.json())  --breaking code
     .then(() => console.log('updated!'));
-const main = function () {
+  
+  const item = store.items[0];
+  console.log('current name: ' + item.name);
+  store.findAndUpdate(item.id, { name: 'foobar' });
+  console.log('new name: ' + item.name);
   
   shoppingList.bindEventListeners();
   shoppingList.render();
